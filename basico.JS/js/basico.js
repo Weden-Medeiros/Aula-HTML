623d13f943a6d4defc207d9e4f5536f33f45ae0c
Weden-Medeiros <Wedenbsb@gmail.com>
2023-07-13 21:54:38 -0300

    Java Script

---
+++ basico.JS/js/basico.js
@@ -0,0 +1,20 @@
+console.log("Hello World, no arquivo js!");
+
+console.log (1+ "1");
+console.log(1-"1");
+
+var x = 10
+let y = 11
+
+var x = 11
+//let y = 11
+
+console.log(typeof(x));
+console.log("Aula"* 2);
+
+let numero = Number("50");
+let numeroString = String(50)
+
+console.log (typeof(numero))
+console.log(typeof(numeroString))
+console.log(typeof(casaDecimais))
